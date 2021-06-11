095488fa47008d078807be5ab0dbf0341159fdb9
Wesley <wes.beck21@outlook.com>
2021-06-11 16:25:51 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e3e09eeb0fb295907d473b816553528'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ae0e4b7-3d1a-4c48-aead-2cb7fb9717a6'
+};
